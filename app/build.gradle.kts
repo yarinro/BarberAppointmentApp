b717d8e1bcf7cffd364f8bfb838e2682dedbc34c
Yarin Rosenfeld <yarin144@gmail.com>
2026-02-03 15:16:42 +0200

    feat: Navgraph configured successfully. next - set ID's for each button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
